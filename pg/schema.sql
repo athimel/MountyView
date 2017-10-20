3ee125d7eb6b136b2763577a28047e16a0b64dcc
Arnaud Thimel <thimel@codelutin.com>
2017-10-20 22:03:37 +0200

    Poussage de la vue en base

--- pg/schema.sql
+++ pg/schema.sql
@@ -1,10 +1,10 @@
+DROP TABLE voit;
+DROP TABLE monstre;
 DROP TABLE grants;
 DROP TABLE update;
 DROP TABLE troll;
 DROP TABLE users;
 DROP TABLE guilde;
-
-
 
 --DROP TABLE guilde;
 CREATE TABLE guilde (
@@ -47,7 +47,7 @@ CREATE TABLE update (
     CONSTRAINT scripts CHECK (script = 'SP_Vue2' OR script = 'SP_Caract' OR script = 'SP_Profil2')
 );
 
--- insert into update (troll, type, date, by) values ( 104259, 'SP_Caract', now(), 'DevelZimZoum');
+-- insert into update (troll, script, date, by) values ( 104259, 'SP_Caract', now(), 'DevelZimZoum');
 
 -- DROP TABLE grants;
 CREATE TABLE grants (
@@ -59,5 +59,34 @@ CREATE TABLE grants (
     CONSTRAINT grant_unique UNIQUE(granted, troll)
 );
 
--- insert into grants (date, granted, troll) values ( now(), 'Wawa', 104259);
+-- insert into grants (date, granted, troll) values (now(), 'Wawa', 104259);
 -- insert into grants (date, granted, troll) values (now(), 'DevelZimZoum', 50362),(now(), 'DevelZimZoum', 86132),(now(), 'DevelZimZoum', 86133),(now(), 'DevelZimZoum', 88222),(now(), 'DevelZimZoum', 90568),(now(), 'DevelZimZoum', 95636),(now(), 'DevelZimZoum', 100160);
+
+
+-- DROP TABLE monster;
+CREATE TABLE monster (
+    id integer PRIMARY KEY,
+    nom text NOT NULL,
+    pos_x integer,
+    pos_y integer,
+    pos_n integer,
+    base_name text,
+    family text,
+    base_nival integer,
+    template text,
+    template_bonus integer,
+    age text,
+    age_bonus integer,
+    nival integer
+);
+
+-- DROP TABLE voit;
+CREATE TABLE voit (
+    id BIGSERIAL REFERENCES update(id),
+    monstre integer REFERENCES monster(id),
+    pos_x integer NOT NULL,
+    pos_y integer NOT NULL,
+    pos_n integer NOT NULL,
+    distance integer NOT NULL,
+    CONSTRAINT voit_pk PRIMARY KEY (id, monstre)
+);
---
+++ vue_to_pg.js
@@ -0,0 +1,154 @@
+const http = require('http');
+const { Client, Pool } = require('pg');
+
+let computeDistance = function(pos1, pos2) {
+
+    let xDistance = Math.abs(pos1.x - pos2.x);
+    let yDistance = Math.abs(pos1.y - pos2.y);
+    let nDistance = Math.abs(pos1.n - pos2.n);
+
+    let result = {
+        horizontalDistance: Math.max(xDistance, yDistance),
+        verticalDistance: nDistance
+    };
+    result.distance = Math.max(result.horizontalDistance, result.verticalDistance);
+
+    return result;
+
+};
+
+let pushViewToDb = function(trollId) {
+
+    let rawUrl = `http://127.0.0.1:3001/?id=${trollId}`;
+    console.log(`Appel de l'URL ${rawUrl}...`);
+
+    let someRequest = http.request(rawUrl, function (response) {
+        let data = '';
+        response.on('data', function (chunk) {
+            data += chunk;
+        });
+
+        response.on('end', function () {
+
+            let json = JSON.parse(data);
+
+            const pool = new Pool({
+              user: 'postgres',
+              host: 'localhost',
+              database: 'postgres',
+              password: 'perso',
+              port: 9999,
+            });
+
+            let parser = require('./monsters.js');
+
+//      id integer PRIMARY KEY,
+//      nom text NOT NULL,
+//      posX integer,
+//      posY integer,
+//      posN integer,
+//      baseName text,
+//      family text,
+//      baseNival integer,
+//      template text,
+//      templateBonus integer,
+//      age integer,
+//      ageBonus integer,
+//      nival integer
+
+
+            console.log(`Insertion de ${json.monstres.length} monstres ...`);
+            for (let monstre of json.monstres) {
+
+                let enriched = parser.parseMonster(monstre);
+                enriched.family = enriched.family || "?";
+                enriched.baseNival = enriched.baseNival || -1;
+
+                // console.log("insert troll " + troll.id);
+                pool.connect((err, client, release) => {
+                  if (err) {
+                    return console.error('Error acquiring client', err.stack)
+                  }
+                  let query = 'INSERT INTO monster (id, nom, pos_x, pos_y, pos_n, base_name, family, base_nival, template, template_bonus, age, age_bonus, nival) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) ON CONFLICT (id) DO UPDATE SET nom=EXCLUDED.nom, pos_x=EXCLUDED.pos_x, pos_y=EXCLUDED.pos_y, pos_n=EXCLUDED.pos_n, base_name=EXCLUDED.base_name, family=EXCLUDED.family, base_nival=EXCLUDED.base_nival, template=EXCLUDED.template, template_bonus=EXCLUDED.template_bonus, age=EXCLUDED.age, age_bonus=EXCLUDED.age_bonus, nival=EXCLUDED.nival;';
+                  let values = [enriched.id, enriched.name, enriched.pos.x, enriched.pos.y, enriched.pos.n, enriched.baseName, enriched.family, enriched.baseNival, enriched.template, enriched.templateBonus, enriched.age, enriched.ageBonus, enriched.nival];
+                  console.log('INSERT INTO monster: ' + enriched.id + '/' + enriched.name);
+                  client.query(query, values, (err, result) => {
+                    release();
+                    if (err) {
+                      return console.error('Error executing query', err.stack);
+                    }
+                    // console.log(result.rows)
+                  });
+
+
+                  let d = computeDistance(json.origine.pos, enriched.pos);
+
+//    id BIGSERIAL PRIMARY KEY REFERENCES update(id),
+//    monstre integer REFERENCES monster(id),
+//    pos_x integer NOT NULL,
+//    pos_y integer NOT NULL,
+//    pos_n integer NOT NULL,
+//    distance integer NOT NULL
+
+                  let query2 = 'INSERT INTO voit (id, monstre, pos_x, pos_y, pos_n, distance) values ($1, $2, $3, $4, $5, $6) ON CONFLICT (id, monstre) DO UPDATE SET pos_x=EXCLUDED.pos_x, pos_y=EXCLUDED.pos_y, pos_n=EXCLUDED.pos_n, distance=EXCLUDED.distance;';
+                  let values2 = [1, enriched.id, enriched.pos.x, enriched.pos.y, enriched.pos.n, d.distance];
+                  console.log('INSERT INTO monster: ' + enriched.id + '/' + enriched.name);
+                  client.query(query2, values2, (err, result) => {
+                    release();
+                    if (err) {
+                      return console.error('Error executing query', err.stack);
+                    }
+                    // console.log(result.rows)
+                  });
+
+                });
+
+            }
+
+//            console.log(`Insertion de ${json.trolls.length} trolls ...`);
+//            for (let troll of json.trolls) {
+//
+//                let propsSet = "{ " +
+//                    "posX: $pos.x, " +
+//                    "posY: $pos.y, " +
+//                    "posN: $pos.n " +
+//                    "}";
+//                tx.run(
+//                    'MERGE (m:Troll {id: $id})' +
+//                    ' ON CREATE SET m+= ' + propsSet +
+//                    ' ON MATCH  SET m+= ' + propsSet,
+//                    troll);
+//
+//                let estVuId = '' + troll.id;
+//                if (estVuId !== trollId) {
+//
+//                    let d = computeDistance(json.origine.pos, troll.pos);
+//
+//                    tx.run('MATCH (voit:Troll {id: $voitId}), (estVu:Troll {id: $estVuId}) ' +
+//                        'MERGE (voit)-[r:VOIT]->(estVu) ' +
+//                        ' ON CREATE SET r+= {quand: $date, posX: $posX, posY: $posY, posN: $posN, distance: $distance}' +
+//                        ' ON MATCH  SET r+= {quand: $date, posX: $posX, posY: $posY, posN: $posN, distance: $distance}',
+//                        {
+//                            estVuId: estVuId,
+//                            voitId: trollId,
+//                            date: date,
+//                            posX: troll.pos.x,
+//                            posY: troll.pos.y,
+//                            posN: troll.pos.n,
+//                            distance: d.distance
+//                        });
+//                }
+//            }
+
+        });
+
+    });
+    someRequest.end();
+
+};
+
+//pushViewToDb('104259');
+//pushViewToDb('88222');
+pushViewToDb('50362');
+
+
